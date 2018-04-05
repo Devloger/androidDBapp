c383eb70e1cafc3f1f6af6cf9309f85787f10478
Devloger <Devloger22@gmail.com>
2018-04-05 21:10:42 +0200

    Dziala Select, Delete, Add! Brakuje tylko Update dla Studenta

--- app/src/main/java/com/example/admin/androiddatabase3/Tab1Studenci.java
+++ app/src/main/java/com/example/admin/androiddatabase3/Tab1Studenci.java
@@ -18,6 +18,7 @@ import android.widget.EditText;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -68,12 +69,20 @@ public class Tab1Studenci extends Fragment implements OnClickListener {
 
 
 
-        ListView studenciListView = (ListView) getView().findViewById(R.id.lista);
+        studenciListView = (ListView) getView().findViewById(R.id.lista);
         List<Student> studenci = Student.listAll(Student.class);
 
 //        studenciListaAdapter = new ArrayAdapter(getActivity(), R.layout.studenci_list_view_layout, R.id.textView ,studenci);
 
-        studenciListView.setAdapter(studenciListaAdapter);
+        studenciListView.setAdapter(new MyListAdaper(getContext(), R.layout.studenci_list_view_layout, studenci));
+//        studenciListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                System.out.println("keke");
+//            }
+//        });
+
+//        studenciListView.setAdapter(studenciListaAdapter);
 
 
 
@@ -133,5 +142,51 @@ public class Tab1Studenci extends Fragment implements OnClickListener {
                 System.out.println("usuwa");
                 break;
         }
+    }
+
+
+
+    private class MyListAdaper extends ArrayAdapter<Student> {
+        private int layout;
+        private List<Student> mObjects;
+        private MyListAdaper(Context context, int resource, List<Student> objects) {
+            super(context, resource, objects);
+            mObjects = objects;
+            layout = resource;
+        }
+
+        @Override
+        public View getView(final int position, View convertView, ViewGroup parent) {
+            ViewHolder mainViewholder = null;
+            if(convertView == null) {
+                LayoutInflater inflater = LayoutInflater.from(getContext());
+                convertView = inflater.inflate(layout, parent, false);
+                ViewHolder viewHolder = new ViewHolder();
+                viewHolder.title = (TextView) convertView.findViewById(R.id.textView);
+                viewHolder.button = (Button) convertView.findViewById(R.id.usun);
+                convertView.setTag(viewHolder);
+            }
+            mainViewholder = (ViewHolder) convertView.getTag();
+            mainViewholder.button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Student studento = Student.findById(Student.class, mObjects.get(position).getId());
+                    studento.delete();
+
+//                    System.out.println("Oke");
+//                    Toast.makeText(getContext(), "Button was clicked for list item " + position, Toast.LENGTH_SHORT).show();
+                }
+            });
+            mainViewholder.title.setText(mObjects.get(position).toString());
+            mainViewholder.id = mObjects.get(position).getId();
+
+            return convertView;
+        }
+    }
+    public class ViewHolder {
+
+        TextView title;
+        Button button;
+        long id;
     }
 }
